226a207f5da91b3552d85ef3336cb92bf847de6b
litner <josesp417@hotmail.com>
2017-02-10 00:03:08 -0800

    Add touch switch (#3)
    * added touch switch functionality
    
    * fixed touch switch
    
    * Updated the Readme
    
    responsibilities and added touch switch
    
    * removed digital write

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <errno.h>
 #include <wiringPi.h>
+#define DOpin 22
 #define RelayPin1 17
 #define RelayPin2 27
-#define DOpin 22
+#define TouchPin 18
 
 int main(void) {
 
@@ -12,7 +13,10 @@ int main(void) {
     return 1;
   }
 
+  int temp = 0;
+
   pinMode(DOpin, INPUT);
+  pinMode(TouchPin, INPUT);
   pinMode(RelayPin1, OUTPUT);
   pinMode(RelayPin2, OUTPUT);
   digitalWrite(RelayPin1, LOW);
@@ -22,6 +26,11 @@ int main(void) {
     if(digitalRead(DOpin)) {
       digitalWrite(RelayPin1, HIGH);
       delay (10000);
+    }
+    else if (digitalRead(TouchPin) != temp) {
+      digitalWrite(RelayPin1, HIGH);
+      temp = digitalRead(TouchPin);
+      delay(10000);
     }
     else {
       digitalWrite(RelayPin1, LOW);
