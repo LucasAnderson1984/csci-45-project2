4f049d5bde7822420f83cfb77845ed2de6c8c4c1
litner <josesp417@hotmail.com>
2017-02-09 23:03:48 -0800

    Add main (#2)
    * added basic sample code elements to main
    
    haphazardly
    
    * Added functionality
    
    Stripped away the sample code and added proper instructions for
    controlling two relays and a photoresistor
    
    * removed math.h & updated readme
    
    * changed main.cpp to main.c
    
    * changed delay & turned off relay when photoresistor off
    
    * added some stuff
    
    * Fixed a line

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
+#include <errno.h>
 #include <wiringPi.h>
-#include <pcf8591.h>
-#include <math.h>
-#define PCF 120
-#define DOpin 0
+#define RelayPin1 17
+#define RelayPin2 27
+#define DOpin 22
 
-int main() {
-  printf("Hello World");
+int main(void) {
+
+  if(wiringPiSetupGpio() < 0) {
+    fprintf(stderr, "Unable to setup wiringPi:%s\n",strerror(errno));
+    return 1;
+  }
+
+  pinMode(DOpin, INPUT);
+  pinMode(RelayPin1, OUTPUT);
+  pinMode(RelayPin2, OUTPUT);
+  digitalWrite(RelayPin1, LOW);
+  digitalWrite(RelayPin2, HIGH);
+
+  while(1) {
+    if(digitalRead(DOpin)) {
+      digitalWrite(RelayPin1, HIGH);
+      delay (10000);
+    }
+    else {
+      digitalWrite(RelayPin1, LOW);
+      delay (500);
+    }
+  }
 
   return 0;
 }
